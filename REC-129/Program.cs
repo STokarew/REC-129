55adc2ff6c0619985a1adf32c50bb29ef391a958
сергей токарев <sseregatokarew@gmail.com>
2022-02-16 18:43:18 +0700

    Добавьте файлы проекта.

---
+++ REC-129/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public static class ArrExtension
+{
+    public static string RandString(this string str, int c)
+    {
+        char[] arr = str.ToCharArray();
+        Random rand = new Random();
+        arr = arr.OrderBy(x => rand.Next()).ToArray();
+        string finalStr = new string(arr);
+        try 
+        {
+            finalStr = finalStr.Substring(0, c - 1);
+            return finalStr;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение: {ex.Message}");
+            Console.WriteLine($"Метод: {ex.TargetSite}");
+            Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
+            Console.ReadKey();
+            return null;
+        }
+    }
+}
+namespace REC_129
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string beginString = Console.ReadLine();
+            Console.Write("Введите длину результирующей строки: ");
+            int len = int.Parse(Console.ReadLine());
+            Console.Write(beginString.RandString(len));
+            Console.ReadKey();
+        }
+    }
+}
